c1ef6e91774a82144962e964e877ff1d99cdf0ee
Julien Schueller <schueller@phimeca.com>
2023-09-27 14:43:26 +0200

    f2018ts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-set "FFLAGS=-std=f2018"
+set "FFLAGS=-std=f2008ts"
 cmake -LAH -G Ninja -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" . || exit 1
 
 cmake --build . --target install --parallel %CPU_COUNT% || exit 1
